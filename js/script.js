7ecc279a99f3cc789d5ce97d4a827ee032cafd0e
joacopichetto <joaquinpichetto@gmail.com>
2023-10-03 19:25:03 -0300

    3/10

---
+++ js/script.js
@@ -0,0 +1,85 @@
+
+
+
+
+typeof(5)
+"number"
+typeof("5")
+"string"
+
+
+3+3  8*3
+//6  //24
+7-3  9/3
+//4  //3
+
+"tengo" + 18 + "años"
+
+
+let nombreDeLaVariable = suValor;
+
+let nombre = 'Mery'
+
+'Hola ' + nombre + ', ¿cómo estas?'
+
+let dineroEnLaCuenta = 100;
+let nuevoDeposito = 1200;
+let comisionBancaria = 0.05;
+(dineroEnLaCuenta+nuevoDeposito)* comisionBancaria
+
+
+"Mi nombre es "+ nombre + " , tengo" + edad + " y estoy cursando en el" + stage + "."
+
+
+let edad = parseInt(prompt("Ingrese su edad"))
+// El usuario ingresará un String con su edad y ParseInt lo convertirá en un número.
+let cumpleaños = edad + 1
+// Se define una Variable con una operación matemática para saber cuántos años tendrá en
+un año.
+alert("En un año tendrás "+ cumpleaños + " año de edad")
+// Le darás un mensaje con la respuesta.
+
+
+let edad= prompt("Ingrese su edad")
+undefined
+edad
+"36"
+//String//
+undefined
+edad= edad+1
+"361"
+
+
+Number("4")
+4
+// Pasa el String "4" a un número 4.
+Number("4.5")
+4.5
+
+parseFloat("4.5$")
+4.5
+
+let mensaje =
+<div>
+    <p>¡Hola!</p>
+</div>
+
+console.log(mensaje)
+
+`string ${variable u operación}`
+
+
+
+// de la nueva forma
+let nombre = 'Gabriela'
+let saludo = `¡Hola, ${nombre}!`
+console.log( saludo )
+// ¡Hola, Gabriela!
+
+
+let a = 5
+let b = 6
+let suma = `La suma es igual a ${a + b}`
+console.log(suma)
+// La suma es igual a 11
+
